0076d5c0a1a6c679f11e1a3420180d4fb46c10af
Skemmatkeelson <Skemmatkeelson@gmail.com>
2021-12-05 04:38:30 +0000

    adds arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+#accepts an input from a user and converts it into an integer
+a=int(input("enter the value of a: "))
+b=int(input("enter the value of b: "))
+
+#addition
+print(f"the sum of {a} and {b} is {a+b}")
+
+#multiplication
+print(f"the product of {a} and {b} is {a*b}")
+
+#subtraction
+print(f"the difference of {a} and {b} is {a-b}")
+
+#division
+print(f"the quotient of {a} and {b} is {a/b}")
+
+#modulus
+print(f"the modulus of {a} and {b} is {a%b}")
+
+#power
+print(f"{a} exponent {b} is {a**b}")
+
+#integer division
+print(f"{a} divided by {b} is {a//b}")
