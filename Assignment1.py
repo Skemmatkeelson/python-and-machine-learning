531d6f4e599049b2ef425254fe9b276ed97d4352
Skemmatkeelson <Skemmatkeelson@gmail.com>
2021-12-06 00:04:16 +0000

    computes area

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+#a python program that accepts the base and height of a triangle and computes the area
+#takes and converts the height into an integer
+base=int(input("enter the value for the base: "))
+height=int(input("enter the value for the height: "))
+
+print(f"the Area of a triangle with base {base} and height {height} is {0.5*base*height}")
